2edcd2eaa030690c06a521ffba8eb3c90e35046a
jbcguidech-crypto <jbc.guide.ch@gmail.com>
2026-07-27 21:50:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Courses & Tarifs — service worker (réseau d'abord pour les pages) */
-const CACHE = 'guidepro-v126';
+const CACHE = 'guidepro-v127';
 const CORE = ['./', './index.html', './manifest.webmanifest?v=guidepro', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
